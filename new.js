6597050e490c1af9550c31cafb5a867823197344
CaioTakabatake <74073758+CaioTakabatake@users.noreply.github.com>
2022-07-30 00:02:43 -0300

    testando protótipos

--- new.js
+++ new.js
@@ -1,19 +1,29 @@
-function User(nome, email) {
-    this.nome = nome;
-    this.email = email;
+// function User(nome, email) {
+//     this.nome = nome;
+//     this.email = email;
 
-    this.exibirInfos = () => {
-        return `${this.nome}, ${this.email}`;
+//     this.exibirInfos = () => {
+//         return `${this.nome}, ${this.email}`;
+//     }
+// }
+
+// function Admin(role) {
+//     User.call(this, 'Caio', 'caio@email.com');
+//     this.role = role || 'estudande';
+// }
+
+// Admin.prototype = Object.create(User.prototype);
+// const novoUser = new Admin('admin');
+
+// console.log(novoUser.exibirInfos());
+// console.log(novoUser.role);
+
+const user = {
+    exibirInfos(nome) {
+        return nome;
     }
 }
 
-function Admin(role) {
-    User.call(this, 'Caio', 'caio@email.com');
-    this.role = role || 'estudande';
-}
-
-Admin.prototype = Object.create(User.prototype);
-const novoUser = new Admin('admin');
-
-console.log(novoUser.exibirInfos());
-console.log(novoUser.role);
+const novoUser = Object.create(user);
+console.log(novoUser.exibirInfos('Caio'));
+console.log(user.isPrototypeOf(novoUser));
