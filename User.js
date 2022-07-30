7541a66fa59e8c4ddffbf82aa471fdb0c51393b7
CaioTakabatake <74073758+CaioTakabatake@users.noreply.github.com>
2022-07-30 02:12:05 -0300

    criando getters

--- User.js
+++ User.js
@@ -12,18 +12,13 @@ export default class User {
         this.#ativo = ativo;
     }
 
-    #montaObjUser() {
-        return ({
-            nome: this.#nome,
-            email: this.#email,
-            nascimento: this.#nascimento,
-            role: this.#role,
-            ativo: this.#ativo
-        });
-    }
+    get nome() { return this.#nome; }
+    get email() { return this.#email; }
+    get nascimento() { return this.#nascimento; }
+    get role() { return this.#role; }
+    get ativo() { return this.#ativo; }
 
-    exibeInfos() {
-        const objUser = this.#montaObjUser();
-        return `${objUser.nome}, ${objUser.email}, ${objUser.nascimento}, ${objUser.role}, ${objUser.ativo}`;
+    exibirInfos() {
+        return `${this.nome}, ${this.email}, ${this.nascimento}, ${this.role}, ${this.ativo}`
     }
 }
