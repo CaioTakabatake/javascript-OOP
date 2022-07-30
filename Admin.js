15be30fad8f1efbea181fdfdc182c0b2ce9ce6c0
CaioTakabatake <74073758+CaioTakabatake@users.noreply.github.com>
2022-07-30 00:59:25 -0300

    adicionando método exclusivo para um Admin

--- Admin.js
+++ Admin.js
@@ -4,8 +4,13 @@ class Admin extends User {
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {
         super(nome, email, nascimento, role, ativo);
     }
+
+    criarCurso(nomeDoCurso, vagas) {
+        return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`;
+    }
 }
 
 const novoAdmin = new Admin('Caio', 'caio@email.com', '2022-01-01');
 console.log(novoAdmin);
 console.log(novoAdmin.exibeInfos());
+console.log(novoAdmin.criarCurso('JavaScript', 300));
