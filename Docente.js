96af3d4e1d11608388d9684a0bfe559175dc71ad
CaioTakabatake <74073758+CaioTakabatake@users.noreply.github.com>
2022-07-30 01:02:44 -0300

    alterando o cargo do Docenete de admin para docenete

--- Docente.js
+++ Docente.js
@@ -1,7 +1,7 @@
 import User from "./User.js";
 
 class Docente extends User {
-    constructor(nome, email, nascimento, role = 'admin', ativo = true) {
+    constructor(nome, email, nascimento, role = 'docente', ativo = true) {
         super(nome, email, nascimento, role, ativo);
     }
 }
