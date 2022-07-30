f83a01255bd0fad192a303731cafcfceeab8e8b4
CaioTakabatake <74073758+CaioTakabatake@users.noreply.github.com>
2022-07-30 00:42:43 -0300

    criando contructor da classe Admin e passando para a classe super

--- Admin.js
+++ Admin.js
@@ -1,5 +1,7 @@
 import User from './User.js';
 
 class Admin extends User {
-
+    constructor(nome, email, nascimento, role = 'admin', ativo = true) {
+        super(nome, email, nascimento, role, ativo);
+    }
 }
