83727384c02df185b240a66f8c4878d5032d22f5
CaioTakabatake <74073758+CaioTakabatake@users.noreply.github.com>
2022-07-30 01:23:59 -0300

    exportando classes Docente e Admin

--- Admin.js
+++ Admin.js
@@ -1,6 +1,6 @@
 import User from './User.js';
 
-class Admin extends User {
+export default class Admin extends User {
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {
         super(nome, email, nascimento, role, ativo);
     }
--- Docente.js
+++ Docente.js
@@ -1,6 +1,6 @@
 import User from "./User.js";
 
-class Docente extends User {
+export default class Docente extends User {
     constructor(nome, email, nascimento, role = 'docente', ativo = true) {
         super(nome, email, nascimento, role, ativo);
     }
