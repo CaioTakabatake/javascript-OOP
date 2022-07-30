40bbb83d0fe5721f6e082e9e9e0361d0a822309b
CaioTakabatake <74073758+CaioTakabatake@users.noreply.github.com>
2022-07-30 01:04:21 -0300

    criando função aprovaEstudante para Docente

--- Docente.js
+++ Docente.js
@@ -4,4 +4,8 @@ class Docente extends User {
     constructor(nome, email, nascimento, role = 'docente', ativo = true) {
         super(nome, email, nascimento, role, ativo);
     }
+
+    aprovaEstudante(estudante, curso) {
+        return `estudante ${estudante} passou no curso ${curso}.`
+    }
 }
