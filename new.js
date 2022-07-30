cb6161d467df65e211e277820de0614fc39e2797
CaioTakabatake <74073758+CaioTakabatake@users.noreply.github.com>
2022-07-29 23:57:45 -0300

    utilizando new para criar um admin que é um user

---
+++ new.js
@@ -0,0 +1,19 @@
+function User(nome, email) {
+    this.nome = nome;
+    this.email = email;
+
+    this.exibirInfos = () => {
+        return `${this.nome}, ${this.email}`;
+    }
+}
+
+function Admin(role) {
+    User.call(this, 'Caio', 'caio@email.com');
+    this.role = role || 'estudande';
+}
+
+Admin.prototype = Object.create(User.prototype);
+const novoUser = new Admin('admin');
+
+console.log(novoUser.exibirInfos());
+console.log(novoUser.role);
