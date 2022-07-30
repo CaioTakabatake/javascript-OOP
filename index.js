d901f8592f81cc2813b03f4e8398b2feb8d8470f
CaioTakabatake <74073758+CaioTakabatake@users.noreply.github.com>
2022-07-30 02:00:58 -0300

    pegando infos pelo objeto privado

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import User from './User.js';
 import Admin from './Admin.js';
 import Docente from './Docente.js';
 
+// const novoUser = new User('Caio', 'caio@email.com', '2022-01-01');
+// console.log(novoUser.exibeInfos());
+// novoUser.nome = 'Rodolfo';
+// console.log(novoUser.exibeInfos());
+
 const novoUser = new User('Caio', 'caio@email.com', '2022-01-01');
-console.log(novoUser.exibeInfos());
-novoUser.nome = 'Rodolfo';
 console.log(novoUser.exibeInfos());
